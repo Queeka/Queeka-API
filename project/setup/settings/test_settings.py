a92a3b17155657c3ae7235b04a5d21ce74fb16a4
AlphaCodz <adeyanjuabiona69@gmail.com>
2024-04-29 17:38:24 +0100

    Allowed_hosts

--- project/setup/settings/test_settings.py
+++ project/setup/settings/test_settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["queeka-api-production.up.railway.app"]
 
 
 # Application definition
