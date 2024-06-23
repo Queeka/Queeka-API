e6963164cec881865f168fc9234f35224334c732
Israel Abiona <adeyanjuabiona69@gmail.com>
2024-06-23 01:53:46 +0100

    new timeout

--- setup/celery.py
+++ setup/celery.py
@@ -14,6 +14,13 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
+app.conf.update(
+    broker_connection_retry_on_startup=True,
+    worker_cancel_long_running_tasks_on_connection_loss=True,
+    worker_timeout=300,
+)
+worker_timeout = timedelta(minutes=5)
+
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
